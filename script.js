bd3afd6532e7990d51ccc391c02dd3aef508343e
Hudsonsilva0987 <Hudsonsilva0987@gmail.com>
2022-08-20 15:38:20 -0300

    Pegadinha att 2.0

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 
 function getRandomArbitrary(min, max) {
-    return  (Math.random() * (max - min) + min) *2  ;
+    return  (Math.random() * (max - min) + min)   ;
   }
 
 function mexe(){
-    document.getElementById("nao").style.left =getRandomArbitrary(0, 200) + "px";
-    document.getElementById("nao").style.top =getRandomArbitrary(0, 200) + "px";
-    document.getElementById("nao").style.right =getRandomArbitrary(0, 200) + "px"
-    document.getElementById("nao").style.bottom =getRandomArbitrary(0, 200) + "px";
+    document.getElementById("nao").style.left =getRandomArbitrary(0, 70) + "%";
+    document.getElementById("nao").style.top =getRandomArbitrary(0, 70) + "%";
+    document.getElementById("nao").style.right =getRandomArbitrary(0, 70) + "%"
+    document.getElementById("nao").style.bottom =getRandomArbitrary(0, 70) + "%";
 }
 
 
